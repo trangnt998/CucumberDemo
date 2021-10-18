6ac0dd085fababf7b4dfde4c58dea8d689810a26
trangnt998 <trangnt998@gmail.com>
2021-10-18 08:14:53 +0700

    *Update report of Cucumber

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,24 +1,34 @@
 package runner;
 
 
-import com.vimalselvam.cucumber.listener.Reporter;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.After;
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import com.cucumber.listener.ExtentCucumberFormatter;
+import com.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import java.io.File;
+import java.io.IOException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/Features",
         glue = "steps",
-        plugin = { "target/reports/report.html"},
+        plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
         monochrome = true
 )
 public class TestRunner {
-    @After
-    public void AfterScenario() {
+
+
+
+
+    @AfterClass
+    public static void AfterScenario() throws IOException {
+        Reporter.addScreenCaptureFromPath("target/cucumber-reports/image");
         Reporter.loadXMLConfig(new File("config/report.xml"));
     }
 }
